3a06166572a89a7db3089b1cbcfef2364f961435
ppedvLukasK <lukask@ppedv.de>
2024-08-12 14:02:43 +0200

    M002 - M004

---
+++ M003/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace M003;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void Button_Clicked(object sender, EventArgs e)
+	{
+		//DisplayAlert: Dem Benutzer eine Frage stellen
+		bool result = await DisplayAlert("Eingabe speichern?", "Möchtest du die Eingabe speichen?", "Ja", "Nein");
+		if (result)
+			Output.Text = Eingabe.Text;
+	}
+}
---
+++ M004/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+
+namespace M004;
+
+public partial class MainPage : ContentPage
+{
+	public ObservableCollection<string> TestItems { get; set; } = new();
+
+	public MainPage()
+	{
+		InitializeComponent();
+		for (int i = 0; i < 10; i++)
+		{
+			TestItems.Add(i.ToString());
+		}
+	}
+
+	private void SearchStarted(object sender, EventArgs e)
+	{
+
+	}
+
+	private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+	{
+
+	}
+
+	private void Switch_Toggled(object sender, ToggledEventArgs e)
+	{
+
+	}
+
+	private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+	{
+
+	}
+}
