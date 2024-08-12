1673d7fa2bfc8ee15d703de0c8a2cf500ed7e10f
ppedvLukasK <lukask@ppedv.de>
2024-08-12 16:52:06 +0200

    M006

---
+++ M006/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace M006;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		double fs = (double) this.Resources["GlobalFontSize"];
+		this.Resources["GlobalFontSize"] = ++fs;
+	}
+}
